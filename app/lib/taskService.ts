065060e8e4852a3c42d2247a9e7c089ed2464cf3
Yolk-Again <joetuu24122008@gmail.com>
2026-03-16 19:29:14 +0700

    Add initial Next.js project files

---
+++ app/lib/taskService.ts
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const API_URL = 'http://localhost:4000/task';
+
+export const fetchTasks = async () => {
+    try {
+        const response = await axios.get(API_URL);
+
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching tasks:', error);
+        return [];
+    }
+};
